beafdc6cb2a43f169f17c152bd8122060b8e1fa5
Yemi <training@thecornelis.com>
2024-11-14 09:29:03 -0600

    add file

--- terraform/s4.tf
+++ terraform/s4.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "secure_bucket" {
-  bucket = "secure-terraform-s3-bucket"
+  bucket = "secure-terraform-s3-bucket-aao"
   acl    = "private"
 
   tags = {
-    Name = "Secure S3 Bucket"
+    Name = "Secure S3 Bucket1"
   }
 }
 
