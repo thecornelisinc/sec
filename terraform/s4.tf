c05bc6cc84b55ec9420485d19f803433fbbb820d
Yemi <training@thecornelis.com>
2024-11-14 09:21:21 -0600

    add file

---
+++ terraform/s4.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "secure-terraform-s3-bucket"
+  acl    = "private"
+
+  tags = {
+    Name = "Secure S3 Bucket"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "secure" {
+  bucket                  = aws_s3_bucket.secure_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
